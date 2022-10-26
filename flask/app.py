54403d8310838fdc795f7f09036c4020875c8070
yenchia189929 <jennifer189929@gmail.com>
2022-10-27 00:29:45 +0800

    add login route

--- flask/app.py
+++ flask/app.py
@@ -1,51 +1,120 @@
+from re import A
 from flask import Flask, render_template, request, url_for, redirect, session
 from pymongo import MongoClient
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+login_manager = LoginManager()
+
 import subprocess, bcrypt
+
 #####################################################################
 
 client = MongoClient(host = "localhost", port = 27017)
 db = client.Project
 
 #####################################################################
-app=Flask(__name__)
 
-## v
+app=Flask(__name__)
+app.secret_key = "app"
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'login'
+
+#####################################################################
+
+# global
+USERNAME = ""
+
+#####################################################################
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+	# if email not in users
+	if len(list(db.USER.find({"email": email}))):
+		user = User()
+		user.id = email
+		return user
+	return
+
+
+@login_manager.request_loader
+def request_loader(request):
+	email = request.form.get('email')
+	
+	if len(list(db.USER.find({"email": email}))):
+		user = User()
+		user.id = email
+		return user
+	return
+
+#####################################################################
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+	print("username = " + USERNAME)
+	return render_template("index.html", username = USERNAME)
 
-## get password for run mdadm command as admin
 @app.route("/login", methods=["GET", "POST"])
 def login():
-	# if "email" in session:
-	# 	return redirect(url_for("logged_in"))
+
 	if request.method == "GET":
+		if current_user.is_authenticated:
+			return redirect(url_for("index"))
 		return render_template("login.html")
 	
 	email = request.form["email"]
 	password = request.form["password"]
-	
-	# password = request.form["password"]
+
+	if len(list(db.USER.find({"email": email}))):
+		password_val = db.USER.find({"email": email})[0]["password"]
+		if bcrypt.checkpw(password.encode('utf-8'), password_val):
+			user = User()
+			user.id = email
+			login_user(user)
+			global USERNAME
+			USERNAME = db.USER.find({"email": current_user.id})[0]["username"]
+			return redirect(url_for("index"))
+
+	return render_template("login.html", msg = "invalid password or email")
 	#subprocess.Popen("python3 ./mydatabase/test.py", shell=True)
-	# return redirect(url_for("index"))
 
-
-# get password for run mdadm command as admin
+# register
 @app.route("/register", methods=["GET", "POST"])
 def register():
 	if request.method == "GET":
 		return render_template("register.html")
+
 	username = request.form["username"]
 	email = request.form["email"]
 	password = request.form["password"]
+
 	if len(list(db.USER.find({"email": email}))) > 0:
-		msg = "email is used"
-		print(len(list(db.USER.find({"email": email}))))
-		print(list(db.USER.find({"email": email})))
-		return render_template("register.html", msg = msg)
+		email_msg = "email is used"
+		return render_template("register.html", email_msg = email_msg, username = username, password = password)
 	
 	db.USER.insert_one({"username": username, "email": email, "password": bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())})
+	user = User()
+	user.id = email
+	login_user(user)
+	global USERNAME
+	USERNAME = db.USER.find({"email": current_user.id})[0]["username"]
 	return redirect(url_for("index"))
+
+# logout
+@app.route("/logout")
+def logout():
+	logout_user()
+	global USERNAME
+	USERNAME = ""
+	return render_template('index.html')
+
+# product
+@app.route("/product")
+def product():
+	return render_template('product.html', username = USERNAME)
+
 
 
 
